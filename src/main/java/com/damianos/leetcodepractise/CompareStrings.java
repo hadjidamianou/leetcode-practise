5925b97e358f03bb00fbe62451c854d0f3ff7f21
psideris <psideris1989@gmail.com>
2022-11-01 09:11:13 +0000

    Askiseis

---
+++ src/main/java/com/damianos/leetcodepractise/CompareStrings.java
@@ -0,0 +1,16 @@
+package com.damianos.leetcodepractise;
+
+public class CompareStrings {
+
+    /*
+     * H methodos tsekarei an 2 string einai idia kai kanei ignore ta uppercase kai lowercase
+     *
+     * s1 = Damianos  s2 = DAMIANos  => return true
+     * s1 = Damian    s2 = DAMIa     => return false
+     * s1 = ""        s2 = ""        => return true
+     * s1 = null      s2 = ""        => return false
+     */
+    public static boolean compare(String s1, String s2) {
+        return false;
+    }
+}
---
+++ src/main/java/com/damianos/leetcodepractise/Palindrome.java
@@ -0,0 +1,28 @@
+package com.damianos.leetcodepractise;
+
+public class Palindrome {
+
+    /*
+     * Palindrome einai mia leksi pou an tin antistrepseis paremenei h idia
+     *
+     * s1 = sos   return true
+     * s1 = mom   return true
+     * s1 = level return true
+     * s1 = kappa return false
+     */
+    public static boolean isPalindrome(String s1) {
+        return false;
+    }
+
+    /*
+     * Bonus askisi
+     * To idio me panw mono pou anti na tsekareis mia leksi tsekareis enan arithmo
+     *
+     * s1 = 1001  return true
+     * s1 = 12321 return true
+     * s1 = 123   return false
+     */
+    public static boolean isPalindromeNumber(int n1) {
+        return false;
+    }
+}
